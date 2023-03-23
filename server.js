0f53dedaac7c0568bbebffe52fe23b7169731f12
faisalbasha19 <108296556+faisalbasha19@users.noreply.github.com>
2023-03-23 15:37:07 +0400

    updated server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World ! Hi New Push 0045');
+  res.send('Hello World ! Hi New Push 4545');
 });
 
 app.listen(PORT, HOST, () => {
